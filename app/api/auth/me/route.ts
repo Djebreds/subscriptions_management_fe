c95f294ebb6d4452ec5816ebba2ceb368113d95e
Djebreds <refiahmad.djebreds@gmail.com>
2025-03-17 12:24:44 +0700

    feat: create route for auth current user

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+}
+
+export async function GET(request: NextRequest) {
+  const accessToken = request.headers.get('Authorization')?.split(' ')[0];
+
+  if (!accessToken) {
+    return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+  }
+
+  try {
+    const apiRes = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/me`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    if (!apiRes.ok) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user: User = await apiRes.json();
+
+    return NextResponse.json({ user });
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong';
+
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
+  }
+}

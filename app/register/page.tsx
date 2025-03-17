e5b82920c55f41e27fe54dfcea0ad340c8b6ceca
Djebreds <refiahmad.djebreds@gmail.com>
2025-03-17 12:25:06 +0700

    feat: create register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -12,26 +12,158 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { z } from 'zod';
+import { toast } from 'sonner';
+import { LoaderSpinner } from '@/components/ui/loader-spinner';
+
+interface Errors {
+  username: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const registerSchema = z
+  .object({
+    firstName: z.string().nonempty({ message: 'First name is required.' }),
+    lastName: z.string().nonempty({ message: 'Last name is required.' }),
+    username: z.string().nonempty({ message: 'Username is required.' }),
+    password: z
+      .string()
+      .nonempty({ message: 'Password is required.' })
+      .min(8, { message: 'Password must be at least 8 characters long.' })
+      .regex(/[A-Z]/, {
+        message: 'Password must contain at least one uppercase letter.',
+      })
+      .regex(/[a-z]/, {
+        message: 'Password must contain at least one lowercase letter.',
+      })
+      .regex(/[0-9]/, { message: 'Password must contain at least one number.' })
+      .regex(/[\W_]/, {
+        message: 'Password must contain at least one special character.',
+      }),
+    confirmPassword: z
+      .string()
+      .nonempty({ message: 'Confirm password is required.' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match.',
+    path: ['confirmPassword'],
+  });
 
 export default function RegisterPage() {
   const router = useRouter();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
+  const [errors, setErrors] = useState<Errors>({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+
+    if (token) {
+      router.push('/dashboard');
+    }
+  }, [router]);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (password !== confirmPassword) {
-      setError('Password do not match.');
+    setErrors({
+      firstName: '',
+      lastName: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    });
+
+    const validationResult = registerSchema.safeParse({
+      firstName,
+      lastName,
+      username,
+      password,
+      confirmPassword,
+    });
+
+    if (!validationResult.success) {
+      const fieldErrors = validationResult.error.flatten().fieldErrors;
+
+      setErrors({
+        firstName: fieldErrors.firstName ? fieldErrors.firstName.join(' ') : '',
+        lastName: fieldErrors.lastName ? fieldErrors.lastName.join(' ') : '',
+        username: fieldErrors.username ? fieldErrors.username.join(' ') : '',
+        password: fieldErrors.password ? fieldErrors.password.join(' ') : '',
+        confirmPassword: fieldErrors.confirmPassword
+          ? fieldErrors.confirmPassword.join(' ')
+          : '',
+      });
+
+      return;
     }
 
-    setError('');
+    setLoading(true);
 
-    router.push('/dashboard');
+    try {
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          username,
+          password,
+          confirmPassword,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (data.user) await handleLogin();
+    } catch (error: unknown) {
+      console.error(error);
+      toast.error('Something went wrong.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await res.json();
+
+      if (data.accessToken) {
+        localStorage.setItem('accessToken', data.accessToken);
+
+        router.push('/dashboard');
+      } else {
+        toast.error(data.error || 'Login failed.', {
+          position: 'top-center',
+        });
+      }
+    } catch (error: unknown) {
+      console.error(error);
+
+      toast.error('Something went wrong.');
+    }
   };
 
   return (
@@ -44,37 +176,90 @@ export default function RegisterPage() {
               <CardDescription>Subscriptions Management</CardDescription>
             </CardHeader>
             <CardContent>
-              <form>
-                <div className='grid gap-6'>
-                  <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
-                    <span className='relative z-10 bg-background px-2 text-muted-foreground'>
-                      Register with
-                    </span>
-                  </div>
+              <div className='grid gap-6'>
+                <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
+                  <span className='relative z-10 bg-background px-2 text-muted-foreground'>
+                    Register with
+                  </span>
+                </div>
+                <form onSubmit={handleSubmit}>
                   <div className='grid gap-6'>
-                    <div className='grid gap-2'>
-                      <Label htmlFor='username'>Username</Label>
-                      <Input
-                        id='username'
-                        type='text'
-                        placeholder='example'
-                        required
-                      />
+                    <div className='flex gap-3'>
+                      <div className='relative'>
+                        <div className='grid gap-2'>
+                          <Label htmlFor='username'>First Name</Label>
+                          <Input
+                            id='firstName'
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            placeholder='Your first name'
+                          />
+                        </div>
+                        {errors.firstName && (
+                          <p className='text-sm text-red-500'>
+                            {errors.firstName}
+                          </p>
+                        )}
+                      </div>
+                      <div className='relative'>
+                        <div className='grid gap-2'>
+                          <Label htmlFor='lastName'>Last Name</Label>
+                          <Input
+                            id='lastName'
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                            placeholder='Your last name'
+                          />
+                        </div>
+                        {errors.lastName && (
+                          <p className='text-sm text-red-500'>
+                            {errors.lastName}
+                          </p>
+                        )}
+                      </div>
                     </div>
-                    <div className='grid gap-2'>
-                      <Label htmlFor='email'>Email</Label>
-                      <Input
-                        id='email'
-                        type='email'
-                        placeholder='mail@example.com'
-                        required
-                      />
+                    <div className='relative'>
+                      <div className='grid gap-2'>
+                        <Label htmlFor='username'>Username</Label>
+                        <Input
+                          id='username'
+                          value={username}
+                          onChange={(e) => setUsername(e.target.value)}
+                          placeholder='Your username'
+                        />
+                      </div>
+                      {errors.username && (
+                        <p className='text-sm text-red-500'>
+                          {errors.username}
+                        </p>
+                      )}
                     </div>
                     <div className='grid gap-2'>
                       <div className='flex items-center'>
                         <Label htmlFor='password'>Password</Label>
                       </div>
-                      <Input id='password' type='password' required />
+                      <div className='relative'>
+                        <Input
+                          id='password'
+                          type={showPassword ? 'text' : 'password'}
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          placeholder='Your password'
+                          className='pr-13'
+                        />
+                        <button
+                          type='button'
+                          onClick={() => setShowPassword((prev) => !prev)}
+                          className='absolute inset-y-0 right-0 flex items-center pr-3 text-sm text-gray-600'
+                        >
+                          {showPassword ? 'Hide' : 'Show'}
+                        </button>
+                      </div>
+                      {errors.password && (
+                        <p className='-mt-2 text-sm text-red-500'>
+                          {errors.password}
+                        </p>
+                      )}
                     </div>
                     <div className='grid gap-2'>
                       <div className='flex items-center'>
@@ -82,23 +267,52 @@ export default function RegisterPage() {
                           Password Confirmation
                         </Label>
                       </div>
-                      <Input id='confirmPassword' type='password' required />
+                      <div className='relative'>
+                        <Input
+                          id='confirmPassword'
+                          type={showConfirmPassword ? 'text' : 'password'}
+                          value={confirmPassword}
+                          onChange={(e) => setConfirmPassword(e.target.value)}
+                          placeholder='Confirm your password'
+                          className='pr-13'
+                        />
+                        <button
+                          type='button'
+                          onClick={() =>
+                            setShowConfirmPassword((prev) => !prev)
+                          }
+                          className='absolute inset-y-0 right-0 flex items-center pr-3 text-sm text-gray-600'
+                        >
+                          {showConfirmPassword ? 'Hide' : 'Show'}
+                        </button>
+                      </div>
+                      {errors.confirmPassword && (
+                        <p className='-mt-2 text-sm text-red-500'>
+                          {errors.confirmPassword}
+                        </p>
+                      )}
                     </div>
-                    <Button type='submit' className='w-full'>
-                      Register
+                    <Button type='submit' className='w-full' disabled={loading}>
+                      {loading ? (
+                        <>
+                          <LoaderSpinner className='text-center' />
+                        </>
+                      ) : (
+                        'Register'
+                      )}
                     </Button>
                   </div>
-                  <div className='text-center text-sm'>
-                    Have an account?{' '}
-                    <Link
-                      href={'/login'}
-                      className='underline underline-offset-4'
-                    >
-                      Sign in
-                    </Link>
-                  </div>
+                </form>
+                <div className='text-center text-sm'>
+                  Have an account?{' '}
+                  <Link
+                    href={'/login'}
+                    className='underline underline-offset-4'
+                  >
+                    Sign in
+                  </Link>
                 </div>
-              </form>
+              </div>
             </CardContent>
           </Card>
           <div className='text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  '>
